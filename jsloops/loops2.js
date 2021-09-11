49b0520c8726df7673f18050c291c6f39c9366d1
uzair <s.uzair.ahmad@hotmail.com>
2021-09-11 14:33:43 -0700

    Loops updated

--- jsloops/loops2.js
+++ jsloops/loops2.js
@@ -2,17 +2,13 @@ var ul = document.getElementById("my-list");
 var button = document.querySelector("button");
 var inputs = document.querySelectorAll("input");
 
-
 button.addEventListener("click", function(){
     var count = inputs[0].value;
     var st = inputs[1].value;
     putItems(st, count);
 });
 
-
-
 console.log(button);
-
 
 function putItems(st, count){
     var innerStr = "";
