6cd01994ebb1d76370e0fcb9c99c0423ed3d7b49
drzeb <aurangzeb.muhammad40@gmail.com>
2021-09-11 21:16:04 -0400

    Doing JS

--- jsloops/loops2.js
+++ jsloops/loops2.js
@@ -1,23 +1,29 @@
-var ul = document.getElementById("my-list");
 var button = document.querySelector("button");
 var inputs = document.querySelectorAll("input");
+var gridContainer = document.querySelector(".grid-container");
+var gridContainerHeader =  document.querySelectorAll(".grid-container .header");
+console.log(gridContainerHeader[0].style.display);
 
 
 button.addEventListener("click", function(){
     var count = inputs[0].value;
     var st = inputs[1].value;
+    if(gridContainerHeader[0].style.display === ""){
+        gridContainerHeader[0].style.display = "block";
+        gridContainerHeader[1].style.display = "block"
+    }
     putItems(st, count);
+    inputs[0].value = undefined;
+    inputs[1].value = "";
 });
 
 
 
-console.log(button);
+// console.log(button);
 
 
 function putItems(st, count){
-    var innerStr = "";
-    for(var i = 0; i < count; i++){
-        innerStr = innerStr + "<li>" + st + " " + i + "</li>"
-    }
-    ul.innerHTML = innerStr;
+    var innerStr = gridContainer.innerHTML;
+    innerStr = innerStr + '<div class="item">' + st + "</div>" + '<div class="item">' + count + "</div>" 
+    gridContainer.innerHTML = innerStr;
 }
