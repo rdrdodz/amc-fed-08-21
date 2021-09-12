924676462344e085ef7775fa0e86a81fe5cf175c
drzeb <aurangzeb.muhammad40@gmail.com>
2021-09-11 20:20:34 -0400

    a folder added

---
+++ jslist/loops.js
@@ -0,0 +1,17 @@
+// var i = 100;
+
+// do{
+//     console.log("Item " + i);
+// }while(i < 10);
+
+// while(i < 10){
+//     console.log("Item " + i);
+//     i += 2;
+// }
+
+
+// for(var i = 1; i < 10; i+=2){
+//     console.log("Item " + i);
+// }
+
+console.log("Loop terminated");
