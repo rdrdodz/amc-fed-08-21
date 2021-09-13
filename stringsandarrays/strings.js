17ee713e35ae2ab9a55aa0914daa0621aaf51135
Junaid Taqui <Junaid.Taqui@gmail.com>
2021-09-13 13:16:52 -0700

    more testing required

--- stringsandarrays/strings.js
+++ stringsandarrays/strings.js
@@ -4,15 +4,28 @@ var st = "shdkashjasekjdihaskdaskloasdhsaojcuhkasjkaxujksahx";
 //interchange the 1st vowel with last one, 2nd with the 
 //2ndlast one and so on
 //Remeber string is immutable 
-
+var vowelMap = {    
+    'A': 'U',
+    'E': 'O',
+    'I': 'I',
+    'O': 'E',
+    'U': 'A',
+    'a': 'u',    
+    'e': 'o',    
+    'i': 'i',
+    'o': 'e',
+    'u': 'a'    
+};
 
 function countVowels(st) {
     var count = 0;    
-    if((st !== "undefined") && (st.length > 0)) {
-        var vowelArray = ['a', 'e', 'i', 'o', 'u'];
+    if((st !== undefined) && (st.length > 0)) {
+        //var vowelArray = ['a', 'e', 'i', 'o', 'u'];
         var len = st.length;
+        var vowelList = getObjectKeys(vowelMap);
         for(var i = 0; i < len; i++) {
-            if(containsString(st[i], vowelArray)) {
+            //if(containsString(st[i], vowelArray)) {
+            if(containsStringBinarySearch(st[i], vowelList)) {
                 count++;
             }
         }
@@ -22,7 +35,7 @@ function countVowels(st) {
 
 function containsString(ch, chArray) {
     var result = false;
-    if((chArray !== "undefined") && (chArray.length > 0)) {
+    if((chArray !== undefined) && (chArray.length > 0)) {
         var len = chArray.length;
         for(i = 0; i < len; i++) {
             if(ch === chArray[i]) {
@@ -33,11 +46,45 @@ function containsString(ch, chArray) {
     }
     return result;
 }
+
+function containsStringBinarySearch(ch, chArray) {
+    var result = false;
+    if((chArray !== undefined) && (chArray.length > 0)) {
+        var index = indexOfStringBinarySearch(ch, chArray);
+        result = (index !== -1);
+    }
+    return result;
+}
+
+function indexOfStringBinarySearch(ch, chArray) {
+    var result = -1;
+    if((chArray !== undefined) && (chArray.length > 0)) {
+        var len = chArray.length;        
+        var startIndex = 0;
+        var endIndex = len - 1;
+        while(startIndex <= endIndex) {
+            var length = endIndex - startIndex + 1;
+            var midIndex = startIndex + Math.floor(length / 2);        
+            if(ch === chArray[midIndex]) {
+                result = midIndex;
+                break;
+            }
+            else if(ch < chArray[midIndex]) {
+                endIndex = midIndex - 1;
+            }
+            else {
+                startIndex = midIndex + 1;
+            }
+        }
+    }
+    return result;
+}
+
 console.log("Number of vowels in " + st + " = " + countVowels(st));
 
 function swapVowels(ch) {
     var result = ch;
-    if((ch !== "undefined") && (ch.length == 1)) {
+    if((ch !== undefined) && (ch.length == 1)) {
         var vowelArray = ['a', 'e', 'i', 'o', 'u'];
         var len = vowelArray.length;
         for(var i = 0; i < len; i++) {
@@ -49,20 +96,35 @@ function swapVowels(ch) {
     }
     return result;
 }
-function interchangeVowels(st) {
+
+function getObjectKeys(objMap) {
+    var result = [];
+    if(objMap !== undefined) {
+        for(var key in objMap) {
+            result.push(key);
+        }    
+    }
+    return result;
+}
+
+function interChangeVowels(st) {
     var result = "";
-    if((st !== "undefined") && (st.length > 0)) {
+    if((st !== undefined) && (st.length > 0)) {
         var len = st.length;
         for(var i = 0; i < len; i++) {
-            result += swapVowels(st[i]);
+            //result += swapVowels(st[i]);
+            var existingChar = st[i];
+            var newChar = vowelMap[existingChar];
+            result += (newChar !== undefined) ? newChar : existingChar;
         }
     }
     return result;
 }
-st = "hellouamerica";
+st = "hElloUAmerIca";
 console.log("st: " + st);
-var swappedVowelledSt = interchangeVowels(st);
+var swappedVowelledSt = interChangeVowels(st);
 console.log("st with swapped vowels: " + swappedVowelledSt);
+console.log("st with swapped vowels again: " + interChangeVowels(swappedVowelledSt));
 // console.log("////////////////////////");
 // var str = "aba";//"aa";//"a";//"";//"abbcdba";
 // if(isPalindrome(str)){
