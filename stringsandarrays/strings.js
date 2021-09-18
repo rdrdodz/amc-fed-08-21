f5a5c3260b65d0a459b54660285a37bd2fb97974
Erum <Erum@Waheeds-MBP.engenius.login>
2021-09-18 16:13:24 -0700

    homework Sept12_2021

--- stringsandarrays/strings.js
+++ stringsandarrays/strings.js
@@ -1,25 +1,83 @@
-var st = "shdkashjaskjdhaskdasklasdhsajchkasjkaxjksahx";
+var st = "aeiouzzzzzzaeiou"; //"shdkashjaskjdhaskdasklasdhsajchkasjkaxjksahx";
+
 //count all the vowels 
+function countVowels(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var vowelCount = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                vowelCount++;
+            }
+        }
+    }
+    return vowelCount;
+}
+
+console.log("*******************");
+console.log("VowelCount is: " + countVowels(st));
+
 //interchange the 1st vowel with last one, 2nd with the 
 //2ndlast one and so on
 //Remeber string is immutable 
+function replaceVowels(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var start = -1;
+    var end = str.length;
+
+    while (start <= end) {
+        for (var i = start + 1; i < str.length; i++) {
+            for (var j = 0; j < vowels.length; j++) {
+                if (str[i] === vowels[j]) {
+                    start = i;
+                    i = str.length;
+                    break;
+
+                }
+            }
+        }
+
+        for (var k = end - 1; k > -1; k--) {
+            for (var j = 0; j < vowels.length; j++) {
+                if (str[k] === vowels[j]) {
+                    end = k;
+                    k = -1;
+                    break;
+                }
+            }
+        }
+
+        if (end > start) {
+            var temp = str[start];
+            str = str.substring(0, start) + str[end] + str.substring(start + 1);
+            str = str.substring(0, end) + temp + str.substring(end + 1);
+        }
+    }
+    return str;
+}
+
+console.log("++++++++++++++++++++++++++++++");
+console.log("Vowel str before replace is: " + st);
+console.log("Vowel str after replace is: " + replaceVowels(st));
+
 
 console.log("////////////////////////");
 var str = "aba";//"aa";//"a";//"";//"abbcdba";
-if(isPalindrome(str)){
+if (isPalindrome(str)) {
     console.log("It is a palindrome");
-}else{
+} else {
     console.log("It is not a palindrome");
 }
 
-function isPalindrome(st){
-    var i = 0; 
-    var j = st.length-1;
-    while(i <= j){
-        if(st[i] === st[j]){
+function isPalindrome(st) {
+    var i = 0;
+    var j = st.length - 1;
+    while (i <= j) {
+        if (st[i] === st[j]) {
             i++;
             j--;
-        }else{
+        } else {
             return false;
         }
     }
@@ -28,20 +86,20 @@ function isPalindrome(st){
 console.log("////////////////////////");
 var st = "aba";//"aa";//"a";//"";//"abbcdba";
 //write a loop to see if this is a palindrom
-var i = 0; 
-var j = st.length-1;
-while(i <= j){
-    if(st[i] === st[j]){
+var i = 0;
+var j = st.length - 1;
+while (i <= j) {
+    if (st[i] === st[j]) {
         i++;
         j--;
-    }else{
+    } else {
         break;
     }
 }
 
-if(i > j){
+if (i > j) {
     console.log("It is a palindrome");
-}else{
+} else {
     console.log("It is not a palindrome");
 }
 
@@ -49,22 +107,22 @@ console.log("////////////////////////");
 var st = "jsckahaohsdoohsdojldpadjjsallaohsdoscnbkuhcshasklk";
 var toSearch = "ohsdouhsdklsdjlkjscklsaclaslksalaks";
 var ind = -1;
-do{
-    ind = st.indexOf(toSearch, ind+1);
-    if(ind !== -1){
+do {
+    ind = st.indexOf(toSearch, ind + 1);
+    if (ind !== -1) {
         console.log("String Found at index " + ind);
-    }else{
+    } else {
         console.log("No more incedences of the string");
     }
-}while(ind !== -1);
+} while (ind !== -1);
 
 
 console.log("////////////////////////");
 ////////////////////////
 var st = "  a quIck Brown fox jUmps over the laYy Dog  ".trim();
-var initSt = st.slice(0,5);
-var midSt =  st.slice(5, 20);
-var endSt =  st.slice(-5).toUpperCase();
+var initSt = st.slice(0, 5);
+var midSt = st.slice(5, 20);
+var endSt = st.slice(-5).toUpperCase();
 var len = st.length;
 console.log(initSt);
 console.log(midSt);
