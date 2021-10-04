f0129579c621987134282f81c7cea8e5a0057b5f
drzeb <aurangzeb.muhammad40@gmail.com>
2021-10-03 20:13:33 -0400

    JS Modules

--- pq/class.maxheap.js
+++ pq/class.maxheap.js
@@ -1,4 +1,4 @@
-class MaxHeap{
+export class MaxHeap{
     constructor(){
         this.q = [];
     }
@@ -53,38 +53,5 @@ class MaxHeap{
             ci = (ri < this.q.length && this.q[ri] > this.q[li] ) ? ri : li;
         }
         return pick;
-    }
-}
-
-var myMaxQ = new MaxHeap();
-var biggestOne = document.querySelector(".biggest-one");
-var numberInput = document.getElementById("number-input");
-var enterButton = document.getElementsByName("enter-button")[0];
-var consumeButton = document.querySelector("button[name='consume-button']");
-
-
-enterButton.addEventListener("click", function(){
-    var num = varifyAndMakeNumeric(numberInput.value);
-    myMaxQ.insert(num);
-    var maxNow = myMaxQ.getMax();
-    biggestOne.innerHTML = maxNow;
-});
-
-consumeButton.addEventListener("click", function(){
-    myMaxQ.consume();
-    if(myMaxQ.isEmpty()){
-        biggestOne.innerHTML = "The List is Empty";
-    }else{
-        biggestOne.innerHTML = myMaxQ.getMax();
-    }
-    
-});
-
-function varifyAndMakeNumeric(st){
-    var num = parseInt(st);
-    if(!isNaN(num)){
-        return num;
-    }else{
-        return 0;
     }
 }
