f4614f28475abbf93513ca7bf08b286b19ed0aab
drzeb <aurangzeb.muhammad40@gmail.com>
2021-10-17 21:32:07 -0400

    Please check the code

--- carousal copy/carousal.js
+++ carousal copy/carousal.js
@@ -3,29 +3,34 @@ var sliderImages = document.querySelectorAll(".img-container .slider-img");
 var imgContainer = document.querySelector(".img-container");
 var maxNumOfImages = 10;
 
-var crrInd = 2;
+var crrInd = 1;
 
-
+sliderImages[(crrInd-1+3)%3].style.zIndex = "10";
+sliderImages[crrInd].style.zIndex = "20";
+sliderImages[(crrInd+1+3)%3].style.zIndex = "5";
 
 rightButton.addEventListener("click", function slide(){
     rightButton.removeEventListener("click", slide);
-    sliderImages[crrInd].style.zIndex = 20;
-    sliderImages[(crrInd-1+3)%3].style.zIndex = 10;
-    sliderImages[(crrInd+1)%3].style.zIndex = 5;
-
+    sliderImages[crrInd].classList.add("start-transition");
     sliderImages[crrInd].style.left = "100%";
+
     sliderImages[crrInd].addEventListener("transitionend",function(){
-        crrInd = (crrInd-1+3)%3;
-        sliderImages[crrInd].style.zIndex = 20;
-        sliderImages[(crrInd-1+3)%3].style.zIndex = 10;
-        sliderImages[(crrInd+1)%3].style.zIndex = 5;
-
-        sliderImages[(crrInd+1)%3].style.left = "0";
-
+        sliderImages[crrInd].classList.remove("start-transition");
+        sliderImages[crrInd].style.zIndex = "5";
+        setTimeout(function(){
+            sliderImages[(crrInd+1+3)%3].style.zIndex = "20";
+            setTimeout(() => {
+                sliderImages[(crrInd-1+3)%3].style.zIndex = "10";
+                setTimeout(()=>{
+                    sliderImages[crrInd].style.left = "0";
+                    setTimeout(()=>{
+                        crrInd = (crrInd+1+3)%3;
+                        rightButton.addEventListener("click", slide);
+                    }, 100);
+                }, 100);
+            }, 100);
+        }, 100);
     });
 
-
-
-    
 });
 
