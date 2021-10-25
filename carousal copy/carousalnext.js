aef187e2c92b6aa2f865792ec575dd610b249a2e
drzeb <aurangzeb.muhammad40@gmail.com>
2021-10-24 20:43:05 -0400

    Slider is complete

--- carousal copy/carousalnext.js
+++ carousal copy/carousalnext.js
@@ -4,6 +4,7 @@ var imgContainer = document.querySelector(".img-container");
 var maxNumOfImages = 10;
 
 var crrInd = 1;
+var srcStr;
 
 sliderImages[(crrInd+1+3)%3].style.zIndex = "5";
 sliderImages[crrInd].style.zIndex = "20";
@@ -22,6 +23,12 @@ rightButton.addEventListener("click", slide = function(){
         sliderImages[crrInd].style.zIndex = "5";
         sliderImages[(crrInd-1+3)%3].style.zIndex = "20";
         sliderImages[(crrInd+1+3)%3].style.zIndex = "10";
+
+        srcStr = sliderImages[crrInd].src;
+        srcStrInit = srcStr.slice(0, -5);
+        srcStrMid = srcStr.slice(-5, -4); 
+        srcEnd =srcStr.slice(-4);
+        sliderImages[(crrInd+1+3)%3].src =  srcStrInit + (parseInt(srcStrMid) - 2 + maxNumOfImages)%maxNumOfImages + srcEnd;
         
         sliderImages[crrInd].style.left = "0";
         
--- carousal copy/carousalold.js
+++ carousal copy/carousalold.js
@@ -9,26 +9,36 @@ sliderImages[(crrInd+1+3)%3].style.zIndex = "5";
 sliderImages[crrInd].style.zIndex = "20";
 sliderImages[(crrInd-1+3)%3].style.zIndex = "10";
 
-
-rightButton.addEventListener("click", slide = function(){
+rightButton.addEventListener("click", function slide(){
     rightButton.removeEventListener("click", slide);
     sliderImages[crrInd].classList.add("start-transition");
     sliderImages[crrInd].style.left = "100%";
 
-    sliderImages[crrInd].addEventListener("transitionend", tra = function(){
-        sliderImages[crrInd].removeEventListener("transitionend", tra)
+    sliderImages[crrInd].addEventListener("transitionend",trans = function(){
+        sliderImages[crrInd].removeEventListener("transitionend",trans);
         sliderImages[crrInd].classList.remove("start-transition");
         
         sliderImages[crrInd].style.zIndex = "5";
         sliderImages[(crrInd-1+3)%3].style.zIndex = "20";
         sliderImages[(crrInd+1+3)%3].style.zIndex = "10";
-        
+
         sliderImages[crrInd].style.left = "0";
+
+        ////////////////
+        srcStr = sliderImages[(crrInd-1+3)%3].src; //currently showing
         
+        srcStrInit = srcStr.slice(0, -5);
+        srcStrMid = srcStr.slice(-5, -4); 
+        srcEnd =srcStr.slice(-4);
+        
+        sliderImages[(crrInd+1+3)%3].src = srcStrInit + 
+                                            parseInt(srcStrMid - 1 + maxNumOfImages)%maxNumOfImages +
+                                            srcEnd;
+
+        ///////////////
         crrInd = (crrInd-1+3)%3;
-        console.log("Zeb")
-        console.log(crrInd);
         rightButton.addEventListener("click", slide);
     });
 
 });
+
